c8578927dfb4b0184002fb378bbae3f97bbf0235
Katrin Silde <90237367+katsilde@users.noreply.github.com>
2023-02-06 16:23:31 +0200

    Add files via upload

---
+++ 01_funktsioon/f1.ts
@@ -0,0 +1,11 @@
+function korrutaKahega(arv:number):number{
+return arv*2;
+}
+
+//console.log(korrutaKahega(7));
+
+function kaheKeskmine(arv1:number, arv2:number):number {
+	return (arv1+arv2)/2;
+}
+
+console.log(kaheKeskmine(7, 8));
