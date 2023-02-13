444da26a9fced78b5df13b83742f9ac5fb3b6e5e
Katrin Silde <90237367+katsilde@users.noreply.github.com>
2023-02-13 13:09:56 +0200

    Add files via upload
    kodutöö

---
+++ 01_funktsioon/f4.ts
@@ -0,0 +1,40 @@
+
+function plaadid():number{
+
+    let klaviatuur=require("readline-sync");
+    //hetkel sisesta andmed ühes mõõtühikus!!!
+    let plaat:number=klaviatuur.question("Plaadi laius cm: ");
+    let vuuk:number=klaviatuur.question("Vuugivahe on mm: ");
+    let laius:number=klaviatuur.question("Põranda laius on m: ");
+    let pikkus:number=klaviatuur.question("Põranda pikkus on m: ");
+    
+    //teisendame
+    //plaat=plaat/100;
+    //vuuk=vuuk/1000;
+
+    //arvutame plaatide koguseid
+    let laius_plaadid:number= Math.ceil((laius+vuuk)/(plaat+vuuk));
+    console.log(laius-(-vuuk));
+    let pikkus_plaadid:number= Math.ceil((pikkus+vuuk)/(plaat+vuuk));
+    let plaadid_kokku:number= laius_plaadid*pikkus_plaadid;
+
+    //Palju plaate läheb
+    console.log("\nPikkupidi läheb "+ pikkus_plaadid+" plaati");
+    console.log("Laiupidi läheb "+ laius_plaadid+" plaati");
+    console.log("Vaja läheb vähemalt "+ plaadid_kokku+" plaati");
+    return plaadid_kokku;
+}
+
+console.log(plaadid());
+
+//väljatrükk
+
+/* Plaadi laius cm: 195
+    Vuugivahe on mm: 5
+    P├Ąranda laius on m: 400
+    P├Ąranda pikkus on m: 600
+    4005
+
+    Pikkupidi läheb 4 plaati
+    Laiupidi läheb 3 plaati
+    Vaja läheb vähemalt 12 plaati */
